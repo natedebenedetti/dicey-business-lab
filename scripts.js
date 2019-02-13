60be8e08674ee6c4493750d194714d321b674a04
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-02-13 11:47:03 -0500

    added comments to explain some of my thought process

--- scripts.js
+++ scripts.js
@@ -2,23 +2,23 @@ const mainDiv = $('#main-div');
 let dieBtn = $('#die-button');
 
 dieBtn.click(function () {
-    for(i = 1; i < 7; i++) {
-    let d = new Die(randomNum());
+    for(i = 1; i < 6; i++) { //added to specify a number of die to be placed on the screen (comparable to the game yahtzee).
+    let d = new Die(randomNum()); //adds a new instance of the class Die
     }
     
 });
 
-function randomNum(min, max) {
+function randomNum(min, max) { //random number generating function for use throughout code
     min = 1;
     max = 7;
     return Math.floor(Math.random() * (max - min) + min);
 
 }
 
-class Die {
+class Die { //OOP class Die.
     constructor(die) {
         this.die = die;
-        this.div = $(`<div class=dice>${this.die}</div>`).appendTo('#dice-div');
+        this.div = $(`<div class=dice>${this.die}</div>`).appendTo('#dice-div'); //appends a new div for every instance created.
         
     }
 }
